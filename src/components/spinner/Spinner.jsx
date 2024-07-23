b6a024de5dd963df4b7d2649a09cf19b879d5159
Danil Murzin <danil.murzin.200026@gmail.com>
2024-07-23 22:20:27 +0300

    add spinner, add errror message

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,9 @@
+import spinnerGif from '../../resources/img/1488.gif'
+
+export const Spinner = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <img src={spinnerGif} alt='' />
+    </div>
+  )
+}
